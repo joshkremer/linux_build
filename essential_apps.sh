20c14d5e6a8526e4f92f69a7fee0865e443aa2c2
joshkremer@gmail.com <joshkremer@gmail.com>
2017-08-05 17:04:32 -0700

    setting up git

--- essential_apps.sh
+++ essential_apps.sh
@@ -17,5 +17,6 @@ apt install -y zsh
 apt install -y terminator
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-
+# PLAYING WITH GIT
+# ASDF
 
