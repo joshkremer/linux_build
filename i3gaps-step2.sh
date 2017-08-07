ab7a18d9d57a9bac92eeaddf2f89a6b43bc1465c
joshkremer <joshkremer@gmail.com>
2017-08-07 09:54:54 -0700

    working on rofi

--- i3gaps-step2.sh
+++ i3gaps-step2.sh
@@ -5,4 +5,4 @@ echo "gap_size 10" >> ~/.i3/config
 echo "new_window 1pixel" >> ~/.i3/config
 
 echo "attempting to change dmenu to rofi launcher"
-
+sed -i -e 's/bindsym $mod+d exec dmenu_runi/bindsym $mod+d exec rofi -show run -bg/g' ~/.i3/config
